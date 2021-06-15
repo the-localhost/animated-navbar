b6a16aabfd8afd60478092f31453129f465b0286
Gagan Agrawal <44469150+the-localhost@users.noreply.github.com>
2021-06-16 01:05:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Animates Navigation Items
+const animateNav = (dir1, dir2) => {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${dir1}-${i+1}`, `slide-${dir2}-${i+1}`);
+    });
+}
+
+const toggleNav = () =>{
+    // Toggle: Menu Bars Open/Closed
+    menuBars.classList.toggle('change');
+    // Toggle: Menu Active
+    overlay.classList.toggle('overlay-active');
+    if(overlay.classList.contains('overlay-active')){
+        // Animate In - Overlay
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+        // Animate In - Nav Items
+        animateNav('out', 'in');
+    }else {
+        // Animate Out - Overlay
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+        // Animate In - Nav Items
+        animateNav('in', 'out');
+    }
+}
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
